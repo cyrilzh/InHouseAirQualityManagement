0ff1fff92f4cf35ccf2e0dca905ebfc0d3eb96f3
cyrilzh <cyrilzhou@hotmail.com>
2017-06-11 21:11:09 +0800

    tested G5 with LCD 1602

--- src/G5.ino
+++ src/G5.ino
@@ -1,5 +1,5 @@
 //#include <AltSoftSerial.h>
-#include "SoftwareSerial.h"
+//#include "SoftwareSerial.h"
 #include <LiquidCrystal.h>
 
 // select the pins used on the LCD panel
@@ -9,12 +9,14 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 int lcd_key     = 0;
 int adc_key_in  = 0;
+bool stopbutton = false;
 #define btnRIGHT  0
 #define btnUP     1
 #define btnDOWN   2
 #define btnLEFT   3
 #define btnSELECT 4
 #define btnNONE   5
+
 
 //Intial Panteng G5 data structure
 struct _panteng {
@@ -28,6 +30,8 @@ struct _panteng {
         unsigned char d[20];
 } panteng;
 
+char pm25title[6]="PM2.5";
+
 // read the buttons
 int read_LCD_buttons()
 {
@@ -40,7 +44,11 @@ int read_LCD_buttons()
  if (adc_key_in < 195)  return btnUP; 
  if (adc_key_in < 380)  return btnDOWN; 
  if (adc_key_in < 555)  return btnLEFT;
- if (adc_key_in < 790)  return btnSELECT;  
+ if (adc_key_in < 790)  
+ {
+  stopbutton=true;
+  return btnSELECT;
+  }  
 
  return btnNONE;  // when all others fail, return this...
 }
@@ -54,7 +62,7 @@ void setup()
         Serial1.begin(9600);        //Initial Serial Port
 }
 
-void loop()
+void displayPM25Data()
 {
         unsigned char c;
         char str[100];
@@ -64,7 +72,9 @@ void loop()
         int pm1_0, pm2_5, pm10_0;        //PM1.0、PM2.5、PM10
         int i;
 
-        if (Serial1.available()) {
+        while (!stopbutton){
+          lcd_key=read_LCD_buttons();
+          if (Serial1.available()) {
                 c = Serial1.read();
                 switch (state) {
                 case 0:
@@ -80,28 +90,78 @@ void loop()
                         break;
                 case 2:
                         ((unsigned char *) &panteng)[count++] = c;
-                        sprintf(str, "%02X ", c);
+                        //sprintf(str, "%02X ", c);
                         //Serial.print(str);
                         if (count > 28) {
                                 state = 0;
-                                pm1_0 = panteng.pm1_0[0] * 256 + panteng.pm1_0[1];
+                                //pm1_0 = panteng.pm1_0[0] * 256 + panteng.pm1_0[1];
                                 pm2_5 = panteng.pm2_5[0] * 256 + panteng.pm2_5[1];
-                                pm10_0 = panteng.pm10_0[0] * 256 + panteng.pm10_0[1];
 
-                                sprintf(str, "%d\t%d\t%d\t%d", time++,pm1_0, pm2_5, pm10_0);
-                                Serial.println(str);
+                                lcd.clear();
+                                lcd.setCursor(4,0);            // move cursor to first line "1" and print title
+                                lcd.print(pm25title);
+                                lcd.setCursor(0,1);            // move cursor to second line "1" and 9 spaces over
+                                lcd.print(pm2_5);
+                                
+                                //pm10_0 = panteng.pm10_0[0] * 256 + panteng.pm10_0[1];
 
-                                snprintf(str,16, "PM2.5=%d    ", pm2_5);
-                                //lcd.clear();
-                                for (i = 0; i < strlen(str); i++) {
-                                        lcd.setCursor(i, 0);
-                                        lcd.print(&str[i]);
+                                //sprintf(str, "%d\t%d\t%d\t%d", time++,pm1_0, pm2_5, pm10_0);
+                                //Serial.println(str);
+//                                sprintf(str,16, "PM2.5=%d    ", pm2_5);
+//                                //lcd.clear();
+//                                for (i = 0; i < strlen(str); i++) {
+//                                        lcd.setCursor(i, 0);
+//                                        lcd.print(&str[i]);
                                 }
-                        }
+                        
                         break;
                 default:
                         break;
                 }
         }
+    }
+ 
+}
+
+void loop()
+{
+  
+ 
+ lcd_key = read_LCD_buttons();  // read the buttons
+ switch (lcd_key)               // depending on which button was pushed, we perform an action
+ {
+   case btnRIGHT:
+   {
+    displayPM25Data();
+    stopbutton=false;
+    break;
+     }
+   case btnLEFT:
+     {
+     stopbutton=false;
+     break;
+     }
+   case btnUP:
+     {
+      stopbutton=false;
+      break;
+     }
+   case btnDOWN:
+     {
+      stopbutton=false;
+      break;
+     }
+   case btnSELECT:
+     {
+     lcd.clear();
+     lcd.setCursor(0,0);
+     lcd.print("Push the buttons"); // print a simple message
+     break;
+     }
+     case btnNONE:
+     {
+     break;
+     }
+ }
 }
 
